9c0aa47216bb864adfa9bfcd0ec09a5c7b7013f4
PurityFime <71134621+PurityFime@users.noreply.github.com>
2020-10-25 18:15:20 +0300

    init: upload code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split('');
+  let reversedArray = arr.reverse();
+  let newStr = reversedArray.join('');
+  let number = parseInt(newStr);
+  return number;
+
 }
